c8e16b44902f230715b060e4cbc0dfd0da73e62b
Justus <justadeayo@gmail.com>
2026-08-03 01:54:14 +0100

    Refactor vendorsetup.sh to include directory checks
    Added checks to avoid cloning directories if they already exist.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,20 @@
-git clone --depth=1 https://github.com/Justadeayo/platform_vendor_xiaomi_violet.git -b 16 vendor/xiaomi/violet
-rm -rf kernel/xiaomi/violet && git clone --depth=1 https://github.com/Justadeayo/android_kernel_xiaomi_violet.git -b sixteen kernel/xiaomi/violet
-git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
-# git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
-rm -rf hardware/xiaomi && git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka-no-dolby hardware/xiaomi && rm -rf hardware/xiaomi/packages/DSPVolumeSynchronizer
-rm -rf hardware/dolby && git clone https://github.com/adi8900/hardware_dolby -b lunaris hardware/dolby
+if [ ! -d "vendor/xiaomi/violet" ]; then
+    git clone --depth=1 https://github.com/Justadeayo/platform_vendor_xiaomi_violet.git -b 16 vendor/xiaomi/violet
+fi
+
+if [ ! -d "kernel/xiaomi/violet" ]; then
+    git clone --depth=1 https://github.com/Justadeayo/android_kernel_xiaomi_violet.git -b sixteen kernel/xiaomi/violet
+fi
+
+if [ ! -d "vendor/xiaomi-firmware/violet" ]; then
+    git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
+fi
+
+if [ ! -d "hardware/xiaomi" ]; then
+    git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka-no-dolby hardware/xiaomi
+    rm -rf hardware/xiaomi/packages/DSPVolumeSynchronizer
+fi
+
+if [ ! -d "hardware/dolby" ]; then
+    git clone https://github.com/adi8900/hardware_dolby -b lunaris hardware/dolby
+fi
