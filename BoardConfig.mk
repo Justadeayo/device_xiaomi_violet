08ad59a715288140546c1cb8bbdc0e412834a7d5
Justus <Justadeayo@gmail.com>
2026-09-16 14:11:35 +0100

    violet: BoardConfig: Consolidate kernel additional flags and fix ARM32 cross-compiler
    * Consolidate duplicate TARGET_KERNEL_ADDITIONAL_FLAGS definitions into a single assignment to prevent LLVM flags from overwriting DTC_EXT and HOSTCFLAGS[span_0](start_span)[span_0](end_span)[span_1](start_span)[span_1](end_span).
    * Explicitly pass CROSS_COMPILE_ARM32 path to allow proper binutils invocation during vDSO32 compilation[span_2](start_span)[span_2](end_span)[span_3](start_span)[span_3](end_span).

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,16 +51,13 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 kpti=off
-BOARD_KERNEL_CMDLINE +=  androidboot.vbmeta.avb_version=1.0
+BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/7c4000.sdhci
 BOARD_KERNEL_CMDLINE += user_debug=31 androidboot.verifiedbootstate=green
 BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
-    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
@@ -70,7 +67,14 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 
 KERNEL_CLANG_TRIPLE := CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_CC := CC=clang
-TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 LLVM_IAS=1
+
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    LLVM=1 \
+    LLVM_IAS=1 \
+    CROSS_COMPILE_ARM32=$(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+    KCFLAGS="-Wno-error -Wno-unused-command-line-argument" \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-error -Wno-unused-command-line-argument"
 
 # Platform
 TARGET_BOARD_PLATFORM := sm6150
